fc1f6703b250562c77ed8ae5658a6fc460807807
Amal A M <vectorhero007@gmail.com>
2024-07-08 11:19:27 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomerFormComponent } from './customer-form/customer-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerFormComponent
   ],
   imports: [
     BrowserModule,
