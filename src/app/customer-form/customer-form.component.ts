64479a580d6338b45630c0254a6683dd237abd9a
Amal A M <vectorhero007@gmail.com>
2024-07-09 10:49:56 +0530

    data is getting from the form

--- src/app/customer-form/customer-form.component.ts
+++ src/app/customer-form/customer-form.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./customer-form.component.css']
 })
 export class CustomerFormComponent {
+  submit(f: any) {
+    console.log(f.value);
 
+    let firstName: string = f.value.firstName;
+    let lastName: string = f.value.lastName;
+    let title: string = f.value.title;
+    let company: string = f.value.company;
+    let mail: string = f.value.mail;
+    let phone: number = f.value.phone;
+    let message: string = f.value.message;
+
+    console.log(`Full Name: ${firstName} ${lastName}`);
+    console.log(`Title: ${title}`);
+    console.log(`Company: ${company}`);
+    console.log(`Email: ${mail}`);
+    console.log(`Contact No: ${phone}`);
+    console.log(`Message: ${message}`);
+
+
+    // console.log(firstName.length);
+
+  }
 }
